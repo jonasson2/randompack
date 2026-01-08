e96aee6c49645ddb885a9cc30d66266b2e56cd80
Kristján Jónasson <jonasson@hi.is>
2026-01-08 14:37:13 +0100

    Next to look at is drawing index from high bits in ziggurat

---
+++ tests/RunTestNo128.c
@@ -0,0 +1,16 @@
+// RunTestNo128.c
+#include <stdio.h>
+#include <string.h>
+#include "TestUtil.h"
+
+void TestCreate(void);
+
+int main(int argc, char **argv) {
+  bool verbose = false;
+  for (int i=1; i<argc; i++) {
+    if (strcmp(argv[i], "-v") == 0) verbose = true;
+  }
+  if (verbose) printf("Running TestCreate under simulated no-128 build.\n");
+  TestCreate();
+  return 0;
+}
