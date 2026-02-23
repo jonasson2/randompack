232a61e5fb1e29882c2f383c5d4d75bc79dce45e
Kristján Jónasson <jonasson@hi.is>
2026-02-23 13:18:10 +0000

    still wheels work

---
+++ scripts/build-wheels.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo Remember to git commit and push
+gh workflow run linux.yml
+#gh workflow run linux-arm.yml
+gh workflow run mac-arm.yml
+gh workflow run mac-x86.yml
+gh workflow run windows.yml
