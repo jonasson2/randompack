0d191ae44f091045f8053287bc7606a9db8eb840
Kristján Jónasson <jonasson@hi.is>
2026-03-12 09:38:19 +0000

    testu01

--- examples/TestU01Driver.c
+++ examples/TestU01Driver.c
@@ -16,6 +16,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <unistd.h>
 
 #include "randompack.h"
@@ -41,14 +42,15 @@ static unsigned int GetBits(void) {
 
 static void usage(const char *prog) {
   fprintf(stderr,
-    "Usage: %s [-h] [-e engine] (-s|-c|-b)\n\n"
+    "Usage: %s [-h] [-e engine] [-s seed] (-S|-C|-B)\n\n"
     "Run TestU01 on a randompack RNG.\n\n"
     "Options:\n"
     "  -h          Show this help\n"
     "  -e engine   RNG engine to use (default engine if omitted)\n"
-    "  -s          SmallCrush  (seconds)\n"
-    "  -c          Crush       (minutes to tens of minutes)\n"
-    "  -b          BigCrush    (hours)\n",
+    "  -s seed     Integer seed passed to randompack_seed\n"
+    "  -S          SmallCrush  (seconds)\n"
+    "  -C          Crush       (minutes to tens of minutes)\n"
+    "  -B          BigCrush    (hours)\n",
     prog
   );
 }
@@ -56,14 +58,25 @@ static void usage(const char *prog) {
 int main(int argc, char **argv) {
   int opt;
   enum { NONE, SMALL, CRUSH, BIG } battery = NONE;
-
+  long seed = 1;
+  bool have_seed = false;
   const char *engine = 0;
-  while ((opt = getopt(argc, argv, "e:scbh")) != -1) {
+  while ((opt = getopt(argc, argv, "e:s:SCBh")) != -1) {
     switch (opt) {
       case 'e': engine = optarg; break;
-      case 's': battery = SMALL; break;
-      case 'c': battery = CRUSH; break;
-      case 'b': battery = BIG; break;
+      case 's': {
+        char *end = 0;
+        seed = strtol(optarg, &end, 10);
+        if (!optarg[0] || (end && end[0]) || seed < INT_MIN || seed > INT_MAX) {
+          fprintf(stderr, "bad seed '%s'\n", optarg);
+          return 1;
+        }
+        have_seed = true;
+        break;
+      }
+      case 'S': battery = SMALL; break;
+      case 'C': battery = CRUSH; break;
+      case 'B': battery = BIG; break;
       case 'h':
       default:
         usage(argv[0]);
@@ -79,6 +92,11 @@ int main(int argc, char **argv) {
   rng = randompack_create(engine);
   if (!rng) {
     fprintf(stderr, "failed to create RNG\n");
+    return 1;
+  }
+  if (have_seed && !randompack_seed((int)seed, 0, 0, rng)) {
+    fprintf(stderr, "failed to seed RNG\n");
+    randompack_free(rng);
     return 1;
   }
 
