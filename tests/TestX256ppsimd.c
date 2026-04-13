4921405c36724ab442aa7baa0984f6770da3d984
Kristján Jónasson <jonasson@hi.is>
2026-04-13 13:13:27 +0000

    work on norm-exp

--- tests/TestX256ppsimd.c
+++ tests/TestX256ppsimd.c
@@ -1,4 +1,4 @@
-// Checking x256++simd against x256++.
+// Checking x256++simd against x256++, and x256**simd against x256**.
 #include <stdint.h>
 #include <stdbool.h>
 #include "randompack.h"
@@ -17,47 +17,54 @@ static inline void get_stream_state(uint64_t stream_state[4], int i,
 }
 
 void TestX256ppsimd(void) {
+  char *scalar_engines[] = {"x256++", "x256**"};
+  char *simd_engines[] = {"x256++simd", "x256**simd"};
   int n = 10000;
   int m = 8*n;
   uint64_t *a, *b;
   TEST_ALLOC(a, n);
   TEST_ALLOC(b, m);
-  randompack_rng *rng_pp = create_seeded_rng("x256++", 42);
-  randompack_rng *rng_fast = create_seeded_rng("x256++simd", 42);
-  check_rng_clean(rng_pp);
-  check_rng_clean(rng_fast);
-  bool ok = randompack_uint64(a, (size_t)n, 0, rng_pp);
-  check_success(ok, rng_pp);
-  ok = randompack_uint64(b, (size_t)m, 0, rng_fast);
-  check_success(ok, rng_fast);
-  uint64_t xora = 0, xorb = 0;
-  for (int i = 0; i < n; i++) xora ^= a[i];
-  for (int i = 0; i < m; i += 8) xorb ^= b[i];
-  xCheck(xora == xorb);
-  xCheck(a[n - 1] == b[m - 8]);
-  randompack_free(rng_pp);
-  randompack_free(rng_fast);
-  randompack_rng *rng_fast2 = create_seeded_rng("x256++simd", 42);
-  check_rng_clean(rng_fast2);
-  uint64_t states[8][4];
-  for (int i = 0; i < 8; i++) get_stream_state(states[i], i, rng_fast2);
-  uint64_t xor_pp = 0;
-  for (int i = 0; i < 8; i++) {
-    randompack_rng *r = randompack_create("x256++");
-    check_rng_clean(r);
-    ok = randompack_set_state(states[i], 4, r);
-    check_success(ok, r);
-    ok = randompack_uint64(b, (size_t)n, 0, r);
-    check_success(ok, r);
-    for (int j = 0; j < n; j++) xor_pp ^= b[j];
-    randompack_free(r);
+  for (int k = 0; k < LEN(scalar_engines); k++) {
+    char *scalar_engine = scalar_engines[k];
+    char *simd_engine = simd_engines[k];
+    randompack_rng *rng_scalar = create_seeded_rng(scalar_engine, 42);
+    randompack_rng *rng_simd = create_seeded_rng(simd_engine, 42);
+    check_rng_clean(rng_scalar);
+    check_rng_clean(rng_simd);
+    bool ok = randompack_uint64(a, (size_t)n, 0, rng_scalar);
+    check_success(ok, rng_scalar);
+    ok = randompack_uint64(b, (size_t)m, 0, rng_simd);
+    check_success(ok, rng_simd);
+    uint64_t xor_scalar = 0;
+    uint64_t xor_simd0 = 0;
+    for (int i = 0; i < n; i++) xor_scalar ^= a[i];
+    for (int i = 0; i < m; i += 8) xor_simd0 ^= b[i];
+    xCheck(xor_scalar == xor_simd0);
+    xCheck(a[n - 1] == b[m - 8]);
+    randompack_free(rng_scalar);
+    randompack_free(rng_simd);
+    randompack_rng *rng_simd2 = create_seeded_rng(simd_engine, 42);
+    check_rng_clean(rng_simd2);
+    uint64_t states[8][4];
+    for (int i = 0; i < 8; i++) get_stream_state(states[i], i, rng_simd2);
+    xor_scalar = 0;
+    for (int i = 0; i < 8; i++) {
+      randompack_rng *r = randompack_create(scalar_engine);
+      check_rng_clean(r);
+      ok = randompack_set_state(states[i], 4, r);
+      check_success(ok, r);
+      ok = randompack_uint64(b, (size_t)n, 0, r);
+      check_success(ok, r);
+      for (int j = 0; j < n; j++) xor_scalar ^= b[j];
+      randompack_free(r);
+    }
+    ok = randompack_uint64(b, (size_t)m, 0, rng_simd2);
+    check_success(ok, rng_simd2);
+    uint64_t xor_simd = 0;
+    for (int i = 0; i < m; i++) xor_simd ^= b[i];
+    xCheck(xor_scalar == xor_simd);
+    randompack_free(rng_simd2);
   }
-  ok = randompack_uint64(b, (size_t)m, 0, rng_fast2);
-  check_success(ok, rng_fast2);
-  uint64_t xor_fast = 0;
-  for (int i = 0; i < m; i++) xor_fast ^= b[i];
-  xCheck(xor_pp == xor_fast);
-  randompack_free(rng_fast2);
   FREE(b);
   FREE(a);
 }
